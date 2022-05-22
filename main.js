d0e5ada3b22bb1a1b754eb4629388bb71566175a
Marek Witkowski <marek.witkowski@onet.com.pl>
2022-05-22 13:35:33 +0200

    add mutate function

--- main.js
+++ main.js
@@ -15,20 +15,20 @@ if(localStorage.getItem("bestBrain")){
   for(let i=0;i<cars.length;i++){
     cars[i].brain=JSON.parse(
       localStorage.getItem("bestBrain"));
-    // if(i!=0){
-    //   NeuralNetwork.mutate(cars[i].brain,0.1);
-    // }
+    if(i!==0){
+      NeuralNetwork.mutate(cars[i].brain,0.2);
+    }
   }
 }
 
 const traffic=[
-  new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(2),-300,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(0),-500,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(1),-500,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(1),-700,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(2),-700,30,50,"DUMMY",2),
+  new Car(road.getLaneCenter(1),-100,30,50,"IDLE",2),
+  new Car(road.getLaneCenter(0),-300,30,50,"IDLE",2),
+  new Car(road.getLaneCenter(2),-300,30,50,"IDLE",2),
+  new Car(road.getLaneCenter(0),-500,30,50,"IDLE",2),
+  new Car(road.getLaneCenter(1),-500,30,50,"IDLE",2),
+  new Car(road.getLaneCenter(1),-700,30,50,"IDLE",2),
+  new Car(road.getLaneCenter(2),-700,30,50,"IDLE",2),
 ];
 
 animate();
